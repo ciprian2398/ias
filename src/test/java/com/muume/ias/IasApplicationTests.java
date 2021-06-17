26de6cc620c65eec894bbfa4ac611f5d6d5da4b7
ciprian-nicuta <ciprian.nicuta@muume.com>
2021-06-17 15:36:15 +0300

    Initial commit

---
+++ src/test/java/com/muume/ias/IasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muume.ias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
