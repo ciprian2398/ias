6e399191de507ebeadb2f287995b816a398af16a
ciprian-nicuta <ciprian.nicuta@muume.com>
2021-06-17 18:31:32 +0300

    working prototype clean

--- src/main/java/com/muume/ias/IasApplication.java
+++ src/main/java/com/muume/ias/IasApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IasApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(IasApplication.class, args);
 	}
-
 }
--- src/main/java/com/muume/ias/controller/TestController.java
+++ src/main/java/com/muume/ias/controller/TestController.java
@@ -2,7 +2,6 @@ package com.muume.ias.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
